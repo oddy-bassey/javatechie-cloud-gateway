d206947e262de3a132b462757337747935bd78d4
@revoltcode <oddybassey@gmail.com>
2021-05-15 21:07:45 +0100

    configured spring api gateway. closes #1

---
+++ src/test/java/com/revoltcode/javatechiecloudgateway/JavatechieCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revoltcode.javatechiecloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatechieCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
